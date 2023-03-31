6aa08c0cd4b17e6b7b3b8421194ebafa86810053
ammar-faifi <a@ammarf.com>
2023-03-31 14:40:36 +0300

    format

--- src/physics/physic_object.hpp
+++ src/physics/physic_object.hpp
@@ -1,76 +1,54 @@
 #pragma once
 #include "collision_grid.hpp"
-#include "engine/common/utils.hpp"
 #include "engine/common/math.hpp"
+#include "engine/common/utils.hpp"
 
+struct PhysicObject {
+  // Verlet
+  Vec2 position = {0.0f, 0.0f};
+  Vec2 last_position = {0.0f, 0.0f};
+  Vec2 acceleration = {0.0f, 0.0f};
+  sf::Color color;
 
-struct PhysicObject
-{
-    // Verlet
-    Vec2 position      = {0.0f, 0.0f};
-    Vec2 last_position = {0.0f, 0.0f};
-    Vec2 acceleration  = {0.0f, 0.0f};
-    sf::Color color;
+  PhysicObject() = default;
 
-    PhysicObject() = default;
+  explicit PhysicObject(Vec2 position_)
+      : position(position_), last_position(position_) {}
 
-    explicit
-    PhysicObject(Vec2 position_)
-        : position(position_)
-        , last_position(position_)
-    {}
+  void setPosition(Vec2 pos) {
+    position = pos;
+    last_position = pos;
+  }
 
-    void setPosition(Vec2 pos)
-    {
-        position      = pos;
-        last_position = pos;
-    }
+  void update(float dt) {
+    const Vec2 last_update_move = position - last_position;
+    const Vec2 new_position =
+        position + last_update_move +
+        (acceleration - last_update_move * 40.0f) * (dt * dt);
+    last_position = position;
+    position = new_position;
+    acceleration = {0.0f, 0.0f};
+  }
 
-    void update(float dt)
-    {
-        const Vec2 last_update_move = position - last_position;
-        const Vec2 new_position = position + last_update_move + (acceleration - last_update_move * 40.0f) * (dt * dt);
-        last_position           = position;
-        position                = new_position;
-        acceleration = {0.0f, 0.0f};
-    }
+  void stop() { last_position = position; }
 
-    void stop()
-    {
-        last_position = position;
-    }
+  void slowdown(float ratio) {
+    last_position = last_position + ratio * (position - last_position);
+  }
 
-    void slowdown(float ratio)
-    {
-        last_position = last_position + ratio * (position - last_position);
-    }
+  [[nodiscard]] float getSpeed() const {
+    return MathVec2::length(position - last_position);
+  }
 
-    [[nodiscard]]
-    float getSpeed() const
-    {
-        return MathVec2::length(position - last_position);
-    }
+  [[nodiscard]] Vec2 getVelocity() const { return position - last_position; }
 
-    [[nodiscard]]
-    Vec2 getVelocity() const
-    {
-        return position - last_position;
-    }
+  void addVelocity(Vec2 v) { last_position -= v; }
 
-    void addVelocity(Vec2 v)
-    {
-        last_position -= v;
-    }
+  void setPositionSameSpeed(Vec2 new_position) {
+    const Vec2 to_last = last_position - position;
+    position = new_position;
+    last_position = position + to_last;
+  }
 
-    void setPositionSameSpeed(Vec2 new_position)
-    {
-        const Vec2 to_last = last_position - position;
-        position           = new_position;
-        last_position      = position + to_last;
-    }
-
-    void move(Vec2 v)
-    {
-        position += v;
-    }
+  void move(Vec2 v) { position += v; }
 };
